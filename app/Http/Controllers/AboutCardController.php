94bda0b7e9e2d30320b7ddeed320ed3926061dc1
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-11-22 16:08:34 +0300

    22 tekrar kurulum - format sonrası

--- app/Http/Controllers/AboutCardController.php
+++ app/Http/Controllers/AboutCardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AboutCardController extends Controller
 {
     public function index() {
-        // Listeleme işlemleri
+        // Listeleme işlemleri.
     }
     
     public function create() {
