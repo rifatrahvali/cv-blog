50186a1aee51ba81e13050c6a14a7dcd7add0ec6
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-11-23 03:19:04 +0300

    34 - admin.index taslak tasarım & partials yapıldı.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,24 +14,7 @@
 
 <body>
 
-    <header class="border-bottom lh-1 py-3">
-        <div class="container">
-            <div class="row flex-nowrap justify-content-between align-items-center">
-                <div class="col-4 pt-1">
-                    <a class="link-secondary text-body-emphasis" href="{{ route('cv') }}">Siteye Git</a>
-                </div>
-                <div class="col-4 text-center">
-                    <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">
-                        "In omnibus requiem quaesivi." <br>
-                            — "Her şeyde huzuru aradım."
-                    </a>
-                </div>
-                <div class="col-4 d-flex justify-content-end align-items-center">
-                    <a class="btn btn-sm btn-outline-secondary text-body-emphasis btn-admin" href="#">Admin</a>
-                </div>
-            </div>
-        </div>
-    </header>
+    @include('layouts.partials.admin.header') <!-- Header buradan geliyor -->
 
 
     <!-- Main Content -->
