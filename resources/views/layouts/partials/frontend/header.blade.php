ea973fa8196dbc6cd57e070824e3b0dbcd471971
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-11-26 23:21:08 +0300

    91 - frontend - sayfalar hazırlandı.

--- resources/views/layouts/partials/frontend/header.blade.php
+++ resources/views/layouts/partials/frontend/header.blade.php
@@ -1,8 +1,18 @@
-<!-- <div class="header text-center my-3">
-    <h1>rahvali</h1>
-    <h2>#dev</h2>
-</div> -->
-<div class="header text-center my-3">
-    <h1>{{ $siteSettings->header_title ?? 'Default Title' }}</h1>
-    <h2>{{ $siteSettings->header_subtitle ?? 'Default Subtitle' }}</h2>
+<div class="container p-0">
+    <div class="row align-items-center">
+        <!-- Başlıklar -->
+        <div class="col-12 text-center">
+            <h1 class="mb-1">{{ $siteSettings->header_title ?? 'Default Title' }}</h1>
+            <h2 class="text-muted mt-1">{{ $siteSettings->header_subtitle ?? 'Default Subtitle' }}</h2>
+        </div>
+
+        <!-- Butonlar -->
+        <div class="col-12 text-center">
+            <div class="d-inline-block">
+                <a href="#" class="btn btn-outline-dark mx-2 pages" style="--bs-btn-border-radius: 0; border:2px solid black;">CV</a>
+                <a href="#" class="btn btn-outline-dark mx-2 pages" style="--bs-btn-border-radius: 0; border:2px solid black;">Yazılar</a>
+                <a href="#" class="btn btn-outline-dark mx-2 pages" style="--bs-btn-border-radius: 0; border:2px solid black;">Destek</a>
+            </div>
+        </div>
+    </div>
 </div>
