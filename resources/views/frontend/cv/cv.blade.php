8eb5a4d817e3a5dd1feb8e963f0982584fb84334
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-11-28 16:38:02 +0300

    108 - VIEWS iyileştirmesi - CV - page - template - layout - partial - CARD -

--- resources/views/frontend/cv/cv.blade.php
+++ resources/views/frontend/cv/cv.blade.php
@@ -8,43 +8,43 @@
         <section class="col-md-4 text-center mb-4 mb-md-0">
             <!-- Profil Kartı -->
             <div class="card mb-4" style="width: 100%; border-radius: 0; border: 2px solid black;">
-                @include('components.frontend.welcome.profile-card')
+                @include('cards.cards-frontend.cards_cv.profile-card')
             </div>
 
             <!-- Hakkımda Kartı -->
             <div class="card text-center mt-3 mt-md-0" style="width: 100%; border-radius: 0; border: 2px solid black;">
-                @include('components.frontend.welcome.about-card')
+                @include('cards.cards-frontend.cards_cv.about-card')
             </div>
         </section>
         <section class="col-md-8 mt-md-0">
             <div class="row">
                 <!-- İş Deneyimi Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.experience-card')
+                    @include('cards.cards-frontend.cards_cv.experience-card')
                 </div>
                 <!-- İş Deneyiminden Öğrenimler Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.learned-from-experiences-card')
+                    @include('cards.cards-frontend.cards_cv.learned-from-experiences-card')
                 </div>
             </div>
             <div class="row">
                 <!-- Eğitim Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.education-card')
+                    @include('cards.cards-frontend.cards_cv.education-card')
                 </div>
                 <!-- Eğitimden Öğrenim Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.learned-from-education-card')
+                    @include('cards.cards-frontend.cards_cv.learned-from-education-card')
                 </div>
             </div>
             <div class="row">
                 <!-- Kurs Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.course-card')
+                    @include('cards.cards-frontend.cards_cv.course-card')
                 </div>
                 <!-- Sertifika Kartı -->
                 <div class="col-md-6 mb-3">
-                    @include('components.frontend.welcome.certificate-card')
+                    @include('cards.cards-frontend.cards_cv.certificate-card')
                 </div>
             </div>
 
